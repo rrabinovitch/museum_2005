ab372f1f8206d3b41cf2a32494f38cfcb1c35c97
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-27 10:45:39 -0600

    Fix interaction pattern issues and complete exhibit interest list method

--- lib/museum.rb
+++ lib/museum.rb
@@ -40,94 +40,14 @@ class Museum
       end
       exhibit_interests
     end
+    # 
+    # -{#<Exhibit:0xXXXXXX @name="Gems and Minerals", @cost=0>=>[#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>], #<Exhibit:0xXXXXXX @name="Dead Sea Scrolls", @cost=10>=>[#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>, #<Patron:0xXXXXXX @name="Johnny", @spending_money=5, @interests=["Dead Sea Scrolls"]>, #<Patron:0xXXXXXX @name="Sally", @spending_money=20, @interests=["IMAX"]>], #<Exhibit:0xXXXXXX @name="IMAX", @cost=15>=>[]}
+    # +{#<Exhibit:0xXXXXXX @name="Gems and Minerals", @cost=0>=>[#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>], #<Exhibit:0xXXXXXX @name="Dead Sea Scrolls", @cost=10>=>[#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>, #<Patron:0xXXXXXX @name="Johnny", @spending_money=5, @interests=["Dead Sea Scrolls"]>], #<Exhibit:0xXXXXXX @name="IMAX", @cost=15>=>[#<Patron:0xXXXXXX @name="Sally", @spending_money=20, @interests=["IMAX"]>]}
 
 
 
 
-    -{
-      #<Exhibit:0xXXXXXX @name="Gems and Minerals", @cost=0>=>
-        [#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>],
-      #<Exhibit:0xXXXXXX @name="Dead Sea Scrolls", @cost=10>=>
-        [#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>,
-         #<Patron:0xXXXXXX @name="Johnny", @spending_money=5, @interests=["Dead Sea Scrolls"]>,
-         #<Patron:0xXXXXXX @name="Johnny", @spending_money=5, @interests=["Dead Sea Scrolls"]>],
-      #<Exhibit:0xXXXXXX @name="IMAX", @cost=15>=>[]}
-    +{#<Exhibit:0xXXXXXX @name="Gems and Minerals", @cost=0>=>
-        [#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>],
-      #<Exhibit:0xXXXXXX @name="Dead Sea Scrolls", @cost=10>=>
-        [#<Patron:0xXXXXXX @name="Bob", @spending_money=20, @interests=["Dead Sea Scrolls", "Gems and Minerals"]>,
-         #<Patron:0xXXXXXX @name="Johnny", @spending_money=5, @interests=["Dead Sea Scrolls"]>],
-      #<Exhibit:0xXXXXXX @name="IMAX", @cost=15>=>[#<Patron:0xXXXXXX @name="Sally", @spending_money=20, @interests=["IMAX"]>]}
 
 
-      ### incomplete attempt
-    # exhibit_interests = {}
-    #
-    # @exhibits.each do |exhibit|
-    #   exhibit_interests[exhibit] = []
-    # end
-    #
-    # @exhibits.each do |exhibit|
-    #   @patrons.each do |patron|
-    #     patron.interests.each do |interest|
-    #       if exhibit.name == interest
-    #         exhibit_interests[exhibit] = [patron]
-    #       end
-    #     end
-    #   end
-    # end
-    #
-    # exhibit_interests
-#######
-    # exhibit_interests = {}
-    #
-    # @exhibits.each do |exhibit|
-    #   exhibit_interests[exhibit] = []
-    # end
-    #
-    # exhibit_interests.each do |exhibit, interested_patrons|
-    #   @patrons.each do |patron|
-    #     patron.interests.each do |interest|
-    #       if exhibit.name == interest && exhibit_interests[exhibit].nil?
-    #         exhibit_interests[exhibit] = [patron]
-    #       elsif exhibit.name == interest
-    #         exhibit_interests[exhibit] << patron
-    #       end
-    #     end
-    #   end
-    #
-    #   exhibit_interests
-    # end
-
-########
-    # exhibit_interests = {}
-    #
-    # @exhibits.each do |exhibit|
-    #   @patrons.each do |patron|
-    #     patron.interests.each do |interest|
-    #       if exhibit_interests[exhibit].nil? && exhibit.name == interest
-    #         exhibit_interests[exhibit] = [patron]
-    #       elsif exhibit.name == intersts
-    #         exhibit_interests[exhibit] << patron
-    #       else
-    #         exhibit_interests[exhibit] = []
-    #       end
-    #     end
-    #   end
-    # end
-    # exhibit_interests
-
-
-########
-  #   @exhibits.reduce({}) do |exhibit_interests, exhibit|
-  #     @patrons.each do |patron|
-  #       if patron.intersests.include?(exhibit.name) && exhibit_interests[exhibit].nil?
-  #         exhibit_interests[exhibit] = [patron]
-  #       elsif patron.intersests.include?(exhibit.name) && !exhibit_interests[exhibit].nil?
-  #         exhibit_interests[exhibit] << patron
-  #       end
-  #     end
-  #     exhibit_interests
-  #   end
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -11,16 +11,6 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-
-    @patron_1 = Patron.new("Bob", 20)
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("Gems and Minerals")
-
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("IMAX")
-
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_exists
@@ -50,11 +40,29 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
+
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 
   def test_it_can_admit_patrons
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1 = Patron.new("Bob", 0)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
+
     @dmns.admit(@patron_1)
     @dmns.admit(@patron_2)
     @dmns.admit(@patron_3)
@@ -63,16 +71,34 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_can_list_patrons_by_exhibit_interest
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
-
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
+    @patron_1 = Patron.new("Bob", 0)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    # @dmns.add_exhibit(@gems_and_minerals)
+    # @dmns.add_exhibit(@dead_sea_scrolls)
+    # @dmns.add_exhibit(@imax)
+    #
+    # @dmns.admit(@patron_1)
+    # @dmns.admit(@patron_2)
+    # @dmns.admit(@patron_3)
+
+
+
     exhibit_interests = {@gems_and_minerals => [@patron_1],
-                        @dead_sea_scrolls => [@patron_1, @patron_3, @patron_3],
+                        @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
                         @imax => []}
     assert_equal exhibit_interests, @dmns.patrons_by_exhibit_interest
   end
