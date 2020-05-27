c20d6b94b9aa04818856c1624db8aa6d8854a1d7
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-27 09:28:16 -0600

    Require patron file and add additional set up w new patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
+require './lib/patron'
 require './lib/museum'
 
 class MuseumTest < Minitest::Test
@@ -10,6 +11,13 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
   end
 
   def test_it_exists
