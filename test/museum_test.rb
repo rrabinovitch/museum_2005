994e50eb7f52a3333e512dfadc3d9d55d6cf3813
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-27 09:19:33 -0600

    Set up museum test file

---
+++ test/museum_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+end
