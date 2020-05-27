470711c50785b1afe4b9ce398c1c33eda0850a55
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-27 12:10:05 -0600

    POST-DEADLINE EDITS: Start working on draw lottery winner test and method and confirming nil for when no one interested in exhibit; incomplete

--- lib/museum.rb
+++ lib/museum.rb
@@ -47,4 +47,8 @@ class Museum
       patron.spending_money < exhibit.cost
     end
   end
+
+  def draw_lottery_winner(exhibit)
+    ticket_lottery_contestants(exhibit).sample.name
+  end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
@@ -112,4 +113,32 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
   end
+
+  def test_it_can_draw_a_lottery_winner
+    skip
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1 = Patron.new("Bob", 0)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal true, @dmns.draw_lottery_winner(@dead_sea_scrolls) == "Bob" || @dmns.draw_lottery_winner(@dead_sea_scrolls) == "Johnny"
+  end
 end
+
+
+# The lottery winner is generated randomly based on the
+# =>  available contestants when draw_lottery_winner is called.
+
+# You will need to use a stub to test the announce_lottery_winner
+# =>  method in conjunction with the draw_lottery_winner method.
