72e2425f8ca93736a66d3e55edd0317d0e488a8e
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-28 11:05:27 -0600

    POST-DEADLINE COMMIT: challenge review session

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,11 +1,12 @@
 class Museum
-  attr_reader :name, :exhibits, :patrons, :revenue
+  attr_reader :name, :exhibits, :patrons, :revenue, :patrons_of_exhibits
 
   def initialize(name)
     @name = name
     @exhibits = []
     @patrons = []
     @revenue = 0
+    @patrons_of_exhibits = {}
   end
 
   def add_exhibit(exhibit)
@@ -13,59 +14,105 @@ class Museum
   end
 
   def recommend_exhibits(patron)
-    #refactor if time
     recommendations = []
-    @exhibits.each do |exhibit|
-      patron.interests.each do |interest|
-        if exhibit.name == interest
-          recommendations << exhibit
-        end
-      end
+    @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
     end
-    recommendations
+
+    ### challenge review: refactor #1
+    # recommendations = []
+    # @exhibits.find_all do |exhibit|
+    #   patron.interests.any? do |interest|
+    #     exhibit.name == interest
+    #   end
+    # end
+
+    ### having an if statement is a hint that we should use find_all
+    # recommendations = []
+    # @exhibits.each do |exhibit|
+    #   patron.interests.each do |interest|
+    #     if exhibit.name == interest
+    #       recommendations << exhibit
+    #     end
+    #   end
+    # end
+    # recommendations
   end
 
   def admit(patron)
     @patrons << patron
 
-    # refactor by moving all code below into patron.attend method to be called on here
-    exhibit_interest_objects = []
+    # # refactor by moving all code below into patron.attend method to be called on here
+    # exhibit_interest_objects = []
+    #
+    # @exhibits.each do |exhibit|
+    #   patron.interests.each do |interest|
+    #     if exhibit.name == interest
+    #       exhibit_interest_objects << exhibit
+    #     end
+    #   end
+    # end
+    #
+    # ordered_by_cost = exhibit_interest_objects.sort_by(&:cost).reverse
+    #
+    # ordered_by_cost.each do |exhibit|
+    #   if exhibit.cost < patron.spending_money
+    #     patron.spending_money -= exhibit.cost
+    #     @revenue += exhibit.cost
+    #   end
+    # end
+  end
 
-    @exhibits.each do |exhibit|
-      patron.interests.each do |interest|
-        if exhibit.name == interest
-          exhibit_interest_objects << exhibit
-        end
-      end
-    end
-
-    ordered_by_cost = exhibit_interest_objects.sort_by(&:cost).reverse
-
-    ordered_by_cost.each do |exhibit|
-      if exhibit.cost < patron.spending_money
-        patron.spending_money -= exhibit.cost
-        @revenue += exhibit.cost
-      end
+  def exhibits_by_cost #added during challenge review #test not yet written
+    @exhibits.sort_by do |exhibit|
+      -exhibit.cost
     end
   end
 
   def patrons_by_exhibit_interest
-    @exhibits.reduce({}) do |exhibit_interests, exhibit|
-      exhibit_interests[exhibit] = []
-      @patrons.each do |patron|
-        patron.interests.each do |interest|
-          if exhibit.name == interest
-            exhibit_interests[exhibit] << patron
-          end
-        end
+    # @exhibits.reduce({}) do |exhibit_interests, exhibit|
+    #   exhibit_interests[exhibit] = []
+    #   @patrons.each do |patron|
+    #     patron.interests.each do |interest|
+    #       if exhibit.name == interest
+    #         exhibit_interests[exhibit] << patron
+    #       end
+    #     end
+    #   end
+    #   exhibit_interests
+    # end
+
+
+    @exhibits.reduce({}) do |acc, exhibit|
+      interested_patrons = @patrons.find_all do |patron|
+        patron.interests.include?(exhibit.name)
       end
-      exhibit_interests
+      acc[exhibit] = interested_patrons
+      acc
     end
+
+    # ### challenge walkthrough
+    # patrons_by_exhibit = {}
+    # @exhibits.each do |exhibit|
+    #   patrons_by_exhibit[exhibit] = []
+    #   interested_patrons = @patrons.find_all do |patron|
+    #     patron.interests.include?(exhibit.name)
+    #   end
+    #   patrons_by_exhibit[exhibit] = interested_patrons
+    # end
+    # patrons_by_exhibit
+
+    # #reduce builds a new collection using values of another collection, as opposed to other enums which return some translated version of the passed collection
+    # return value of the block becomes the starting point for the next iteration
   end
 
   def ticket_lottery_contestants(exhibit)
-    @patrons.find_all do |patron|
-      patron.spending_money < exhibit.cost && patron.interests.include?(exhibit.name)
+    # @patrons.find_all do |patron|
+    #   patron.spending_money < exhibit.cost && patron.interests.include?(exhibit.name)
+    # end
+
+    patrons_by_exhibit_interest[exhibit].find_all do |patron|
+      patron.spending_money < exhibit.cost
     end
   end
 
@@ -76,10 +123,13 @@ class Museum
 
   def announce_lottery_winner(exhibit)
     winner = draw_lottery_winner(exhibit)
-    if winner
-      "#{winner} has won the IMAX edhibit lottery"
-    else
-      "No winners for this lottery"
-    end
+    return "No winners for this lottery" if winner.nil?
+    "#{winner} has won the #{exhibit.name} exhibit lottery"
+
+    # if winner
+    #   "#{winner} has won the #{exhibit.name} exhibit lottery"
+    # else
+    #   "No winners for this lottery"
+    # end
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -23,8 +23,9 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_starts_with_no_exhibits_and_no_patrons
-    assert_empty @dmns.exhibits
-    assert_empty @dmns.patrons
+    assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
+    assert_equal ({}), @dmns.patrons_of_exhibits
   end
 
   def test_exhibits_can_be_added
@@ -41,14 +42,14 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 20)
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("IMAX")
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
 
-    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
-    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(patron_2)
   end
 
   def test_it_can_admit_patrons
@@ -56,19 +57,19 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
 
-    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+    assert_equal [patron_1, patron_2, patron_3], @dmns.patrons
   end
 
   def test_it_can_list_patrons_by_exhibit_interest
@@ -76,20 +77,20 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
 
-    exhibit_interests = {@gems_and_minerals => [@patron_1],
-                        @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+    exhibit_interests = {@gems_and_minerals => [patron_1],
+                        @dead_sea_scrolls => [patron_1, patron_2, patron_3],
                         @imax => []}
     assert_equal exhibit_interests, @dmns.patrons_by_exhibit_interest
   end
@@ -99,19 +100,19 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
 
-    assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+    assert_equal [patron_1, patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
   end
 
   def test_there_are_no_contestants_if_none_interested_in_exhibit
@@ -119,16 +120,16 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
     @patron_3 = Patron.new("Johnny", 5)
     @patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
     @dmns.admit(@patron_3)
 
     assert_empty @dmns.ticket_lottery_contestants(@imax)
@@ -139,23 +140,23 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
 
-    lottery_contestants = [@patron_1.name, @patron_3.name]
+    lottery_contestants = [patron_1.name, patron_3.name]
     lottery_winner = @dmns.draw_lottery_winner(@dead_sea_scrolls)
 
     assert_equal true, lottery_contestants.include?(lottery_winner)
-    assert_equal false, lottery_contestants.include?(@patron_2.name)
+    assert_equal false, lottery_contestants.include?(patron_2.name)
     assert_nil @dmns.draw_lottery_winner(@imax)
   end
 
@@ -164,20 +165,20 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @patron_3 = Patron.new("Johnny", 5)
-    @patron_3.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
-    @dmns.admit(@patron_3)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
 
-    @dmns.stubs(:draw_lottery_winner).returns("Bob")
-    announcement = "Bob has won the IMAX edhibit lottery"
+    @dmns.stubs(:draw_lottery_winner).returns(patron_1.name)
+    announcement = "Bob has won the Gems and Minerals exhibit lottery"
     assert_equal announcement, @dmns.announce_lottery_winner(@gems_and_minerals)
   end
 
@@ -186,16 +187,16 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
 
-    @patron_1 = Patron.new("Bob", 0)
-    @patron_1.add_interest("Gems and Minerals")
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("Dead Sea Scrolls")
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
     @patron_3 = Patron.new("Johnny", 5)
     @patron_3.add_interest("Dead Sea Scrolls")
 
-    @dmns.admit(@patron_1)
-    @dmns.admit(@patron_2)
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
     @dmns.admit(@patron_3)
 
     announcement = "No winners for this lottery"
@@ -207,42 +208,71 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@imax)
     @dmns.add_exhibit(@dead_sea_scrolls)
 
-    @tj = Patron.new("TJ", 7)
-    @tj.add_interest("IMAX")
-    @tj.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@tj)
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
 
-    assert_equal 7, @tj.spending_money
-    assert_equal 0, @dmns.revenue
+    assert_equal 7, tj.spending_money
+    # assert_equal 0, @dmns.revenue
 
-    @patron_1 = Patron.new("Bob", 10)
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("IMAX")
-    @dmns.admit(@patron_1)
+    patron_1 = Patron.new("Bob", 10)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("IMAX")
+    @dmns.admit(patron_1)
 
-    assert_equal 10, @patron_1.spending_money
-    assert_equal 10, @dmns.revenue
+    assert_equal 10, patron_1.spending_money
+    # assert_equal 10, @dmns.revenue
 
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("IMAX")
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@patron_2)
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_2.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_2)
 
-    assert_equal 5, @patron_2.spending_money
-    assert_equal 25, @dmns.revenue
+    assert_equal 5, patron_2.spending_money
+    # assert_equal 25, @dmns.revenue
 
-    @morgan = Patron.new("Morgan", 15)
-    @morgan.add_interest("Gems and Minerals")
-    @morgan.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@morgan)
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    @dmns.admit(morgan)
 
-    assert_equal 5, @morgan.spending_money
-    assert_equal 35, @dmns.revenue
+    assert_equal 5, morgan.spending_money
+    # assert_equal 35, @dmns.revenue
   end
 
-
   def test_it_can_list_patrons_by_exhibit
-    skip
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
+
+    patron_1 = Patron.new("Bob", 10)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("IMAX")
+    @dmns.admit(patron_1)
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_2.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_2)
+
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    @dmns.admit(morgan)
+
+    expected = {
+      @gems_and_minerals => [morgan],
+      @dead_sea_scrolls => [patron_1, morgan],
+      @imax => [patron_2]
+    }
+
+    assert_equal expected, @dmns.patrons_of_exhibits
   end
 
   def test_admitting_patrons_increases_revenue
@@ -251,31 +281,31 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@imax)
     @dmns.add_exhibit(@dead_sea_scrolls)
 
-    @tj = Patron.new("TJ", 7)
-    @tj.add_interest("IMAX")
-    @tj.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@tj)
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
 
     assert_equal 0, @dmns.revenue
 
-    @patron_1 = Patron.new("Bob", 10)
-    @patron_1.add_interest("Dead Sea Scrolls")
-    @patron_1.add_interest("IMAX")
-    @dmns.admit(@patron_1)
+    patron_1 = Patron.new("Bob", 10)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("IMAX")
+    @dmns.admit(patron_1)
 
     assert_equal 10, @dmns.revenue
 
-    @patron_2 = Patron.new("Sally", 20)
-    @patron_2.add_interest("IMAX")
-    @patron_2.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@patron_2)
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    patron_2.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_2)
 
     # assert_equal 25, @dmns.revenue
 
-    @morgan = Patron.new("Morgan", 15)
-    @morgan.add_interest("Gems and Minerals")
-    @morgan.add_interest("Dead Sea Scrolls")
-    @dmns.admit(@morgan)
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    @dmns.admit(morgan)
 
     # assert_equal 35, @dmns.revenue
   end
